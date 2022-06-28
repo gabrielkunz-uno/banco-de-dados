94684fc2979d9e12869ff3bdd7d4ff301a85ad53
gabrielkunz-uno <gabriel.kunz@unochapeco.edu.br>
2022-06-28 17:12:53 -0300

    feat: updates

---
+++ updates/atividade.sql
@@ -0,0 +1,8 @@
+-- atualizar o rental_duration de todos os filmes para 10 
+-- para os filmes da categoria 'Comedy'
+
+-- atualizar nome da cidade para 'JAVASCRIPTLANDIA' para 
+-- a cidade com código 180;
+
+-- atualizar o nome do idioma para 'Portugues Brasileiro' 
+-- no lugar de 'Mandarin'
